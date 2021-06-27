88bd5d1ed7710f7ec59b6e15b9435853f2fed506
raghavi101 <56163096+raghavi101@users.noreply.github.com>
2021-06-27 11:34:15 +0530

    Inital enpoints work (#1)
    Co-authored-by: Vedang Joshi <43697446+vedangj044@users.noreply.github.com>

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///./DB.db"
+SQLALCHEMY_DATABASE_URL = "db link"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
-            SQLALCHEMY_DATABASE_URI,
-            connect_args={"check_same_thread": False}
-        )
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from fastapi import FastAPI, Depends, status
-from fastapi.middleware.cors import CORSMiddleware
+#uvicorn main:app --reload
+from typing import List
+
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import models
-from database import SessionLocal, engine
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
 
 app = FastAPI()
 
-# Creates database tables
-models.Base.metadata.create_all(bind=engine)
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app.add_middleware(
         CORSMiddleware,
@@ -18,12 +26,24 @@ app.add_middleware(
         allow_credentials=True,
     )
 
-def get_db():
-    try:
-        db =  SessionLocal()
-        yield db
-    finally:
-        db.close()
+
+@app.get("/profile")
+def profile_details():
+    return {"Hello": "World"}
+
+
+@app.get("/leaderboard")
+def read_item():
+    pass
+
+@app.get("/search")
+def profile_details():
+    pass
+
+@app.get("/star")
+def profile_details():
+    pass
+
 
 @app.post("/api/register/", status_code=status.HTTP_201_CREATED)
 def register(username: str, password: str, db: Session = Depends(get_db)):
@@ -52,3 +72,4 @@ def index():
     return {
         "message": "Hello, World!"
     }
+ 
